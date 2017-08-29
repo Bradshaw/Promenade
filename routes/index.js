c553a8e2f72ec7ff392c1420269668d1151c5f42
Kevin Bradshaw <gaeel@nanoleptic.net>
2017-08-29 02:38:03 +0200

    Basic Rotonde surfer

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,51 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http');
+
+
+var starturl = 'rotonde.xxiivv.com';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {url: starturl});
+});
+
+/* GET home page. */
+router.get('/:url', function(req, res, next) {
+  console.log(req.params.url);
+  http.get('http://'+req.params.url, (hres) => {
+    const { statusCode } = hres;
+    const contentType = hres.headers['content-type'];
+
+    let error;
+    if (statusCode !== 200) {
+      error = new Error('Request Failed.\n' +
+                        `Status Code: ${statusCode}`);
+    }/* else if (!/^application\/json/.test(contentType)) {
+      error = new Error('Invalid content-type.\n' +
+                        `Expected application/json but received ${contentType}`);
+    }*/
+    if (error) {
+      next();
+      // consume response data to free up memory
+      hres.resume();
+      return;
+    }
+
+    hres.setEncoding('utf8');
+    let rawData = '';
+    hres.on('data', (chunk) => { rawData += chunk; });
+    hres.on('end', () => {
+      try {
+        const parsedData = JSON.parse(rawData);
+        res.render('rotonde', parsedData);
+      } catch (e) {
+        next()
+      }
+    });
+  }).on('error', (e) => {
+    next()
+  });
 });
 
 module.exports = router;
