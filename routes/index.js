a0e403261878870be9a74099783240f5b0c1d30b
Kevin Bradshaw <gaeel@nanoleptic.net>
2017-09-10 13:43:48 +0200

    Made robust to http(s?) and malformed rotonde feeds

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,10 @@ var http = require('http');
 var auth = require('basic-auth')
 var async = require('async')
 var request = require('request');
+var fs = require('fs');
+var path = require('path');
+var appDir = path.dirname(require.main.filename);
+appDir = path.join(appDir, "..");
 
 var starturl = 'logs.spaceshipsin.space';
 
@@ -29,51 +33,64 @@ router.get('/post', function(req, res, next) {
 
 /* GET home page. */
 router.get('/:url', function(req, res, next) {
-  http.get(
-    {
-        host: req.params.url,
-        headers: {
-          accept: 'application/json'
-        }
-    }, (hres) => {
-    const { statusCode } = hres;
-    const contentType = hres.headers['content-type'];
-    let error;
-    if (statusCode !== 200) {
-      error = new Error('Request Failed.\n' +
-                        `Status Code: ${statusCode}`);
-    }/* else if (!/^application\/json/.test(contentType)) {
-      error = new Error('Invalid content-type.\n' +
-                        `Expected application/json but received ${contentType}`);
-    }*/
-    if (error) {
-      next();
-      // consume response data to free up memory
-      hres.resume();
-      return;
-    }
-
-    hres.setEncoding('utf8');
-    let rawData = '';
-    hres.on('data', (chunk) => { rawData += chunk; });
-    hres.on('end', () => {
-      try {
-        const parsedData = JSON.parse(rawData);
-        
+  var prefix = 'http://';
+  var url = req.params.url;
+  if (url.substr(0, prefix.length) !== prefix)
+  {
+      url = prefix + url;
+  }
+  const options = {
+    url :  url,
+    json : true
+  };
+  request(options,
+    function(err, rs, parsedData) {
+      console.log(parsedData);
+      fixRotonde(parsedData, req.params.url);
+      if (err){
+        next();
+      } else {
+        parsedData.feed.sort(function(a,b){
+          return b.time-a.time
+        })
         getAll(parsedData.portal, function(feed) {
             parsedData.portalFeed = feed;
             res.render('rotonde', parsedData);
-        });
-        
-      } catch (e) {
-        next()
+        }, [req.params.url]);
       }
-    });
-  }).on('error', (e) => {
-    next()
-  });
+    }
+  );
 });
 
+function fixRotonde(rotonde, url) {
+  var url = url.replace("http://","");
+  var defaultRot = JSON.parse(fs.readFileSync(path.join(appDir, "public", "default.json")));
+  var defaultPost = JSON.parse(fs.readFileSync(path.join(appDir, "public", "post.json")));
+  defaultRot.meta.canonical = url;
+  defaultRot.profile.name = url;
+  for (var v in defaultRot) {
+    if (defaultRot.hasOwnProperty(v)) {
+      rotonde[v] = rotonde.getDef(v,defaultRot[v]);
+    }
+  }
+  for (var v in defaultRot.meta){
+    if (defaultRot.meta.hasOwnProperty(v)){
+      rotonde.meta[v] = rotonde.meta.getDef(v,defaultRot.meta[v]);
+    }
+  }
+  for (var v in defaultRot.profile){
+    if (defaultRot.profile.hasOwnProperty(v)){
+      rotonde.profile[v] = rotonde.profile.getDef(v,defaultRot.profile[v]);
+    }
+  }
+  for (var i = 0; i < rotonde.feed.length; i++) {
+    for (var v in defaultPost){
+      if (defaultPost.hasOwnProperty(v)){
+        rotonde.feed[i][v] = rotonde.feed[i].getDef(v,defaultPost[v]);
+      }
+    }
+  }
+}
 function httpGet(url, callback) {
   var prefix = 'http://';
   if (url.substr(0, prefix.length) !== prefix)
@@ -87,6 +104,7 @@ function httpGet(url, callback) {
   request(options,
     function(err, res, body) {
       if (typeof body == 'object'){
+        fixRotonde(body, url);
         body.url = url;
       }
       callback(err, body);
@@ -94,7 +112,10 @@ function httpGet(url, callback) {
   );
 }
 
-function getAll(rotondes, cb) {
+function getAll(rotondes, cb, ignores) {
+  ignores.map(function(url){
+    return url.replace("http://","");
+  });
   async.map(rotondes, httpGet, function (err, res){
     if (err){
       cb([]);
@@ -104,7 +125,7 @@ function getAll(rotondes, cb) {
       console.log("-------");
       for (var v in res) {
         if (res.hasOwnProperty(v)) {
-          if (typeof res[v] == 'object'){
+          if (typeof res[v] == 'object' && !ignores.includes(res[v].url.replace("http://",""))) {
               var rot = res[v];
               feed = feed.concat(rot.feed.map(function(post){
                 return {
@@ -117,12 +138,26 @@ function getAll(rotondes, cb) {
         }
       }
       feed.sort(function(a, b){
-        return a.post.date-b.post.date;
+        return b.post.time-a.post.time;
       })
       feed = feed.slice(0,30);
       cb(feed);
     }
   });
 }
+Object.prototype.isThing = function(key){
+  if (!this.hasOwnProperty(key)){
+    return false;
+  } else {
+    if (this[key])
+      return true;
+    else
+      return false;
+  }
+}
+Object.prototype.getDef = function (key, def) {
+  return this.isThing(key) ? this[key] : def;
+};
+
 
 module.exports = router;
